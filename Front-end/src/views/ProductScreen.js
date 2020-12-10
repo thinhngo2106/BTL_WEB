22031272358cce508d4bcb8b0c36beb0c9dfbfd6
phuongbui2207 <57555872+phuongbui2207@users.noreply.github.com>
2020-12-10 23:09:13 +0700

    update css

--- Front-end/src/views/ProductScreen.js
+++ Front-end/src/views/ProductScreen.js
@@ -9,8 +9,8 @@ import MessageBox from '../components/MessageBox';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import { addToCart } from '../actions/cartActions';
-
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheckCircle, faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -52,8 +52,21 @@ export default function ProductScreen(props) {
     };
 
 
+
+    // const imageMagnifier = (imgID) => {
+    //     let img = document.getElementById(imgID)	
+    //     let lens = document.getElementById('lens')
+
+	//     lens.style.backgroundImage = `url( ${img.src} )`
+
+	//     let ratio = 3
+
+	//     lens.style.backgroundSize = (img.width * ratio) + 'px ' + (img.height * ratio) + 'px';
+    // }
+
     return (
       <div>
+        <p/>
         {loading ? (
           <LoadingBox></LoadingBox>
         ) : error ? (
@@ -61,32 +74,63 @@ export default function ProductScreen(props) {
         ) : (
         <div className="col-lg-9 col-md-12 col-sm-12 col-xs-12">
             <div className="row">
-                <div className="product-detail-left product-images col-xs-12 col-sm-6 col-md-5 col-lg-5">
+                {/* <div className="product-detail-left product-images col-xs-12 col-sm-6 col-md-5 col-lg-5"> */}
+                <div className="product-detail-left product-images col-xs-12 col-sm-5 col-md-5 col-lg-5">
+                    {/* <div className="lens"/> */}
                     <img className="medium" src={product.productdetail.image} alt={product.productName}/>
+                    {/* <button
+                        onMouseEnter={(imageMagnifier)}
+                        // onMouseLeave={()}
+                    ></button> */}
                 </div>
-                <div className = "col-xs-12 col-sm-6 col-md-7 col-lg-7 details-pro">
-                            <h1 className="titleName">
-                                {product.productName}
-                            </h1>
 
-                            <div >
-                                {product.quantityInStock > 0 ?(
-                                    <span className="productStatus">    Tình trạng: Còn hàng </span>    
-                                ) : (
-                                    <span className="productStatus"> Tình trạng: Hết hàng </span>
-                                )}
-                            </div>
-                            <div className="product-des">
-                                <span>
-                                    {product.productdetail.productDescription}                                
-                                </span>
-                            </div>
-                        <div className="price-box">
-                            <span className="productPrice">
-                                {product.productPrice}
-                            </span>
+                {/* <div className = "col-xs-12 col-sm-6 col-md-7 col-lg-7 details-pro"> */}
+                <div className = "col-xs-12 col-sm-6 col-md-6 col-lg-6 details-pro">
+
+                    <h1 className="titleName">
+                        {product.productName}
+                    </h1>
+
+                    <div >
+                        {product.quantityInStock > 0 ?(
+                            <span className="productStatus"> Tình trạng: Còn hàng </span>    
+                        ) : (
+                        <span className="productStatus"> Tình trạng: Hết hàng </span>
+                        )}
+                        <p/>   
+                    </div>
+
+                    <div className="price-box">
+                        <span className="productPrice">
+                            {product.productPrice}
+                        </span>
+                        <p/>
+                    </div>
+
+                    <div className="product-des">
+                        <span>
+                            {product.productdetail.productDescription}
+                            <p/>                                
+                        </span>
+                    </div>
+                    
+                    <div className="product-policy">
+                        <div className="product-policy-content">
+                                <FontAwesomeIcon icon={faCheckCircle} className="d-inline-block icon"/>
+                                <p className="d-inline-block"> &nbsp; Freeship cho đơn hàng từ 2.000.000đ</p> 
                         </div>
-                        <div>
+                        <div className="product-policy-content">
+                                <FontAwesomeIcon icon={faCheckCircle} className="d-inline-block icon"/>
+                                <p className="d-inline-block"> &nbsp; Bảo hành chính hãng 1 tháng với giày dép</p>
+                        </div>
+                        <div className="product-policy-content">
+                                <FontAwesomeIcon icon={faCheckCircle} className="d-inline-block icon"/>
+                                <p className="d-inline-block"> &nbsp; Freeship cho đơn hàng từ 600k ( tối đa 30k )</p> 
+                        </div>
+                    </div>
+                    <div>
+                        <p/>   
+                        Kích thước: &nbsp;
                         <select className="item-sizes" 
                         value={size}
                         onChange={(e) => setSize(e.target.value)}
@@ -97,27 +141,34 @@ export default function ProductScreen(props) {
                                   </option>
                                 )
                               )}
-                                </select>      
-                        </div>
+                                </select>
+                        <p/>         
+                    </div>
+
                     <div>
-                        <div>
+                        <div className="quantity-button"> Số lượng: &nbsp;
                             <button onClick={decNum} className="dec-Button">  <RemoveIcon/>  </button>
                             <input className="input" type="text" value={qty} onChange={e => setQty(e.target.value)} />
                             <button onClick={incNum} className="inc-Button" > <AddIcon/> </button>
                         </div>
 
+                        <p/> 
+
                         {product.quantityInStock > 0 ?(
-                        <button onClick={addToCartHandler} className="primary block">
-                            Thêm vào giỏ hàng
-                        </button>
+                            <button onClick={addToCartHandler} className="cart">
+                                Thêm vào giỏ hàng
+                            </button>
+                            
+
                         ) : (
-                            <button className="primary block over">
+                            <button className="x-cart">
                             Không thể thêm vào giỏ hàng
                         </button>
-                        )
-
+                        )  
                         }
+                        <p/>     
                     </div>
+                    
                 </div>
 
                 <div>
