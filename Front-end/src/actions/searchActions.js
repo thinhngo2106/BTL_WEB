73d6f0b102dee33b2968feee1ca5148952c1d236
thinhngo2106 <55101812+thinhngo2106@users.noreply.github.com>
2020-12-17 16:42:46 +0700

    update

--- Front-end/src/actions/searchActions.js
+++ Front-end/src/actions/searchActions.js
@@ -1,45 +1,45 @@
 import {
-    SEARCH_KEYWORD_REQUEST,
-    SEARCH_KEYWORD_SUCCESS,
-    SEARCH_KEYWORD_FAIL,
-    SEARCH_CATEGORY_REQUEST,
-    SEARCH_CATEGORY_SUCCESS,
-    SEARCH_CATEGORY_FAIL,
+  SEARCH_KEYWORD_REQUEST,
+  SEARCH_KEYWORD_SUCCESS,
+  SEARCH_KEYWORD_FAIL,
+  SEARCH_CATEGORY_REQUEST,
+  SEARCH_CATEGORY_SUCCESS,
+  SEARCH_CATEGORY_FAIL,
 
 } from "../constants/searchConstants";
 import Axios from "axios";
 
 export const searchKeyword = (queryKey) => async (dispatch) => {
 
-    dispatch({
-      type: SEARCH_KEYWORD_REQUEST, payload: queryKey,
-    });
-    try {      
-      const { data } = await Axios.get('/api/search/', {params:{
-        query: queryKey
-      }});
-      dispatch({ type: SEARCH_KEYWORD_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({type: SEARCH_KEYWORD_FAIL, payload: error.message });
-    }
+  dispatch({
+    type: SEARCH_KEYWORD_REQUEST, payload: queryKey,
+  });
+  try {      
+    const { data } = await Axios.get('/api/search/', {params:{
+      query: queryKey
+    }});
+    dispatch({ type: SEARCH_KEYWORD_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({type: SEARCH_KEYWORD_FAIL, payload: error.message });
+  }
 };
 
 export const searchCategory = (nameCategory, pageNumber) => async (dispatch) => {
+
+dispatch({
+  type: SEARCH_CATEGORY_REQUEST, payload: nameCategory,
+});
+try {      
+  const { data } = await Axios.get('/api/search/categories/', {params:{
+    name: nameCategory,
+    page: pageNumber ? pageNumber : 0,
+  }});
   
-  dispatch({
-    type: SEARCH_CATEGORY_REQUEST, payload: nameCategory,
-  });
-  try {      
-    const { data } = await Axios.get('/api/search/categories/', {params:{
-      name: nameCategory,
-      page: pageNumber ? pageNumber : 0,
-    }});
-    
-    
-    dispatch({ type: SEARCH_CATEGORY_SUCCESS, payload: data});
-  } catch (error) {
-    dispatch({type: SEARCH_CATEGORY_FAIL, payload: error.message });
-  }
+  
+  dispatch({ type: SEARCH_CATEGORY_SUCCESS, payload: data});
+} catch (error) {
+  dispatch({type: SEARCH_CATEGORY_FAIL, payload: error.message });
+}
 };
 
 
