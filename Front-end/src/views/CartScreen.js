96ab8e0935ab013837b6f3902100039acba12e35
phuongbui2207 <57555872+phuongbui2207@users.noreply.github.com>
2020-12-20 23:35:14 +0700

    Update CartScreen.js

--- Front-end/src/views/CartScreen.js
+++ Front-end/src/views/CartScreen.js
@@ -3,9 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import MessageBox from '../components/MessageBox';
 import { addToCart, removeFromCart } from '../actions/cartActions';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 import './css/cartScreen.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Table from 'react-bootstrap/Table'
+
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
@@ -30,6 +32,7 @@ export default function CartScreen(props) {
     const checkoutHandler = () => {
         props.history.push('/signin?redirect=shipping');
       };
+  
 
     return (
         <div className="row cart col-md-10">
@@ -44,62 +47,8 @@ export default function CartScreen(props) {
                     </MessageBox>
                     </div>)
                     : (
-                        // <div className="cart-check">
-                        //     <div className="cart-items col-sm-12 col-md-11">
-                        //         {cartItems.map((item) => (
-                        //             <div className="row cart-item col-sm-12 col-md-11" key={item.id}>
-                        //                 <div className="col col-sm-2 col-md-2">
-                        //                 <Link to={`/product/${item.product}`}>
-                        //                     <img src={item.image}
-                        //                         alt={item.name} className="small"></img>
-                        //                         </Link>
-                        //                 </div> 
-                        //                 <div className="col col-sm-4 col-md-4">
-                        //                     <Link to={`/product/${item.product}`}  style={{textDecoration: 'none'}}>
-                        //                         <span className="item-attribute-name">{item.name}</span></Link>
-                        //                 </div>
-                        //                 <div className="col col-md-2">
-                        //                     <span className="item-attribute-name"  style={{textAlign: 'center'}}>{item.size}</span>    
-                        //                 </div>
-                        //                 <div className="col col-sm-1 col-md-1">
-                        //                     <select className="item-quantity"
-                        //                         value={item.qty}
-                        //                         onChange={(e) =>
-                        //                             dispatch(
-                        //                             addToCart(item.product, Number(e.target.value))
-                        //                             )
-                        //                         }
-                        //                     >
-                        //                     {[...Array(item.countInStock).keys()].map((x) => (
-                        //                         <option key={x + 1} value={x + 1}>
-                        //                         {x + 1}
-                        //                         </option>
-                        //                     ))}
-                        //                     </select>
-                        //                 </div>
-                        //                 <div className="col col-sm-1 col-md-2">
-                        //                     <span className="item-attribute" style={{textAlign: 'center'}}>${item.price}  </span></div>
-                        //                 <div className="col col-sm-2 col-md-1">
-                        //                     <button
-                        //                     className="remove-item"
-                        //                     type="button"
-                        //                     onClick={() => removeFromCartHandler(item.product)}
-                        //                     >
-                        //                     Xóa
-                        //                     </button>
-                        //                 </div>
-                        //             </div>
-                        
-                        
-                            //     ))}
-
-                            // </div>  
-                    // </div>
-                        
                     <div>
-                        {cartItems.map((item) => (
-
-                        <Table striped bordered hover>
+                        <table className="cart-table">
                             <thead>
                                 <tr>
                                 <th>#</th>
@@ -112,6 +61,7 @@ export default function CartScreen(props) {
                                 </tr>
                             </thead>
                             <tbody>
+                                {cartItems.map((item) => (
                                 <tr>
                                 <td></td>
                                 <td>
@@ -136,11 +86,13 @@ export default function CartScreen(props) {
                                             )
                                         }
                                     >
-                                    {[...Array(item.countInStock).keys()].map((x) => (
+                                    {
+                                    [...Array(item.countInStock).keys()].map((x) => (
                                         <option key={x + 1} value={x + 1}>
                                         {x + 1}
                                         </option>
-                                    ))}
+                                    ))
+                                    }
                                     </select>
                                 </td>
                                 <td>
@@ -157,9 +109,10 @@ export default function CartScreen(props) {
                                     </button> 
                                 </td>
                                 </tr>
+                                   ))}
                             </tbody>
-                        </Table>
-                        ))}
+                        </table>
+                     
                     </div>
 
                     )
